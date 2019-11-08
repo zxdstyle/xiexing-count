79cac632eb36893f4302b7a167671e81845086d4
zxdstyle <1430822515@qq.com>
2019-11-08 16:16:38 +0800

    first commit

---
+++ src/Count.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2019-11-08
+ * Time: 16:15
+ */
+
+namespace Zxdstyle\Count;
+
+
+class Count
+{
+
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2019-11-08
+ * Time: 16:14
+ */
+
+namespace Zxdstyle\Count;
+
+
+class ServiceProvider
+{
+    protected $defer = true;
+
+    public function register()
+    {
+        $this->app->singleton(Count::class, function(){
+            return new Count();
+        });
+
+        $this->app->alias(Count::class, 'count');
+    }
+
+    public function provides()
+    {
+        return [Count::class, 'count'];
+    }
+}
